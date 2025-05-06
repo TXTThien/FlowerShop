3bdf1b13b4ffea60ca23a957dbf9c5808646c462
TXTThien <sktththien@gmail.com>
2025-05-06 12:19:19 +0700

    orde

--- src/main/java/org/example/controller/Staff/StaffAccountGiftController.java
+++ src/main/java/org/example/controller/Staff/StaffAccountGiftController.java
@@ -1,6 +1,7 @@
 package org.example.controller.Staff;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.OrderHistory;
 import org.example.entity.*;
 import org.example.entity.enums.Status;
 import org.example.repository.*;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,13 +36,46 @@ public class StaffAccountGiftController {
         List<Discount> discounts = discountRepository.findAll();
         List<Order> orders = orderRepository.findAll();
         List<Gift> gifts = giftRepository.findAll();
+        List<OrderHistory> orderHistories = new ArrayList<>();
+        for (Order order : orders) {
+            OrderHistory orderHistory = new OrderHistory();
+            orderHistory.setOrderID(order.getOrderID());
+            orderHistory.setDate(order.getDate());
+            orderHistory.setIsPaid(order.getPaid());
+            orderHistory.setTotal(order.getTotalAmount());
+            orderHistory.setAddress(order.getDeliveryAddress());
+            orderHistory.setPhone(order.getPhoneNumber());
+            orderHistory.setName(order.getName());
+            orderHistory.setPaid(order.getHadpaid());
+            orderHistory.setConfirm(order.getConfirm());
+            orderHistory.setCondition(order.getCondition());
+            if (order.getNote() != null) {
+                orderHistory.setNote(order.getNote());
+            }
+            if (order.getShipping() != null) {
+                if (order.getShipping().getStartDate() != null) {
+                    orderHistory.setShipStart(order.getShipping().getStartDate());
+                }
+                if (order.getShipping().getCompleteDate() != null) {
+                    orderHistory.setShipEnd(order.getShipping().getCompleteDate());
+                }
+                if (order.getShipping().getNote() != null) {
+                    orderHistory.setShipperNote(order.getShipping().getNote());
+                }
+                orderHistory.setShipperName(order.getShipping().getAccountID().getName());
+                orderHistory.setShipperPhone(order.getShipping().getAccountID().getPhoneNumber());
+                orderHistory.setShipperEmail(order.getShipping().getAccountID().getEmail());
+
+            }
+            orderHistories.add(orderHistory);
+        }
 
         Map<String, Object> response = new HashMap<>();
         response.put("gifts", gifts);
         response.put("accountGifts", accountGifts);
         response.put("accounts", accounts);
         response.put("discounts", discounts);
-        response.put("orders", orders);
+        response.put("orders", orderHistories);
 
         return ResponseEntity.ok(response);
     }
@@ -53,12 +88,45 @@ public class StaffAccountGiftController {
         List<Discount> discounts = discountRepository.findAll();
         List<Order> orders = orderRepository.findAll();
         List<Gift> gifts = giftRepository.findAll();
+        List<OrderHistory> orderHistories = new ArrayList<>();
+        for (Order order : orders) {
+            OrderHistory orderHistory = new OrderHistory();
+            orderHistory.setOrderID(order.getOrderID());
+            orderHistory.setDate(order.getDate());
+            orderHistory.setIsPaid(order.getPaid());
+            orderHistory.setTotal(order.getTotalAmount());
+            orderHistory.setAddress(order.getDeliveryAddress());
+            orderHistory.setPhone(order.getPhoneNumber());
+            orderHistory.setName(order.getName());
+            orderHistory.setPaid(order.getHadpaid());
+            orderHistory.setConfirm(order.getConfirm());
+            orderHistory.setCondition(order.getCondition());
+            if (order.getNote() != null) {
+                orderHistory.setNote(order.getNote());
+            }
+            if (order.getShipping() != null) {
+                if (order.getShipping().getStartDate() != null) {
+                    orderHistory.setShipStart(order.getShipping().getStartDate());
+                }
+                if (order.getShipping().getCompleteDate() != null) {
+                    orderHistory.setShipEnd(order.getShipping().getCompleteDate());
+                }
+                if (order.getShipping().getNote() != null) {
+                    orderHistory.setShipperNote(order.getShipping().getNote());
+                }
+                orderHistory.setShipperName(order.getShipping().getAccountID().getName());
+                orderHistory.setShipperPhone(order.getShipping().getAccountID().getPhoneNumber());
+                orderHistory.setShipperEmail(order.getShipping().getAccountID().getEmail());
+
+            }
+            orderHistories.add(orderHistory);
+        }
         Map<String, Object> response = new HashMap<>();
         response.put("gifts",gifts);
         response.put("accountGift", accountGifts);
         response.put("accounts", accounts);
         response.put("discounts", discounts);
-        response.put("orders", orders);
+        response.put("orders", orderHistories);
 
         return ResponseEntity.ok(response);
     }
